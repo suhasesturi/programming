2da729c39fd88d87d16fb475f782fe21bad053ca
Suhas EJ <suhas235@gmail.com>
2020-03-29 13:35:24 +0530

    InterviewBit Bit Manipulation - Bucketing

--- src/interviewbit/BitManipulation/MinXORValue.java
+++ src/interviewbit/BitManipulation/MinXORValue.java
@@ -1,4 +1,4 @@
-package interviewbit;
+package interviewbit.BitManipulation;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -7,8 +7,8 @@ public class MinXORValue {
     public int findMinXor(ArrayList<Integer> A) {
         Collections.sort(A);
         int result = Integer.MAX_VALUE;
-        for (int i = 0; i < A.size() - 1; i++) {
-            result = Math.min(result, A.get(i) ^ A.get(i + 1));
+        for (int i = 1; i < A.size(); i++) {
+            result = Math.min(result, A.get(i) ^ A.get(i - 1));
         }
         return result;
     }
