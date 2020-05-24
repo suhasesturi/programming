a2ab886034705155d5f00f0070a1ec7a47571cb2
Suhas EJ <suhas235@gmail.com>
2020-05-24 16:07:57 +0530

    codejam 1C

---
+++ src/codejam/Y2020/Sample.java
@@ -0,0 +1,21 @@
+package codejam.Y2020;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Sample {
+	private static final BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws IOException {
+		int t = Integer.parseInt(in.readLine());
+		for (int x = 1; x <= t; x++) {
+			System.out.print("Case #" + x + ": ");
+			solve();
+		}
+	}
+
+	public static void solve() throws IOException {
+
+	}
+}
