f1ca7f5c5bf2928d2fb9b6c7bc5304f08dbde351
Suhas EJ <suhas235@gmail.com>
2020-06-17 15:26:34 +0530

    InterviewBit Stacks and Queues - Multiple Stack

--- src/interviewbit/StacksAndQueues/MinStack.java
+++ src/interviewbit/StacksAndQueues/MinStack.java
@@ -3,29 +3,30 @@ package interviewbit.StacksAndQueues;
 import java.util.Stack;
 
 public class MinStack {
-    private Stack<Integer> elements = new Stack<>();
-    private Stack<Integer> minimum = new Stack<>();
+	private final Stack<Integer> elements = new Stack<>();
+	private final Stack<Integer> minimum = new Stack<>();
 
-    public void push(int x) {
-        elements.push(x);
-        if (minimum.isEmpty() || minimum.peek() >= x) minimum.add(x);
-    }
+	public void push(int x) {
+		if (minimum.isEmpty() || x <= minimum.peek()) {
+			minimum.push(x);
+		}
+		elements.push(x);
+	}
 
-    public void pop() {
-        if (!elements.isEmpty()) {
-            if (minimum.peek().equals(elements.pop())) minimum.pop();
-        }
-    }
+	public void pop() {
+		if (!elements.isEmpty()) {
+			Integer pop = elements.pop();
+			if (!minimum.isEmpty() && minimum.peek().equals(pop)) {
+				minimum.pop();
+			}
+		}
+	}
 
-    public int top() {
-        return elements.isEmpty() ? -1 : elements.peek();
-    }
+	public int top() {
+		return elements.isEmpty() ? -1 : elements.peek();
+	}
 
-    public int getMin() {
-        return minimum.isEmpty() ? -1 : minimum.peek();
-    }
+	public int getMin() {
+		return minimum.isEmpty() ? -1 : minimum.peek();
+	}
 }
-
-
-
-
