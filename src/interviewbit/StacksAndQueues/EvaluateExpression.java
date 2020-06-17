a727be6caa9ac34192396afcaa059e290d90c03a
Suhas EJ <suhas235@gmail.com>
2020-06-17 15:51:11 +0530

    InterviewBit Stacks and Queues - Stack Math

--- src/interviewbit/StacksAndQueues/EvaluateExpression.java
+++ src/interviewbit/StacksAndQueues/EvaluateExpression.java
@@ -5,31 +5,26 @@ import java.util.Stack;
 
 public class EvaluateExpression {
     public int evalRPN(ArrayList<String> A) {
-        Stack<String> expression = new Stack<>();
-        for (String a : A) {
-            if (a.equals("+") || a.equals("-") || a.equals("*") || a.equals("/")) {
-                int first = Integer.parseInt(expression.pop());
-                int second = Integer.parseInt(expression.pop());
-
-                switch (a) {
-                    case "+":
-                        expression.push(String.valueOf(first + second));
-                        break;
-                    case "-":
-                        expression.push(String.valueOf(second - first));
-                        break;
-                    case "*":
-                        expression.push(String.valueOf(first * second));
-                        break;
-                    case "/":
-                        expression.push(String.valueOf(second / first));
-                        break;
-                }
-            } else {
-                expression.push(a);
+        Stack<Integer> stack = new Stack<>();
+        for (String s : A) {
+            switch (s) {
+                case "+":
+                    stack.push(stack.pop() + stack.pop());
+                    break;
+                case "-":
+                    stack.push(-stack.pop() + stack.pop());
+                    break;
+                case "*":
+                    stack.push(stack.pop() * stack.pop());
+                    break;
+                case "/":
+                    int divisor = stack.pop();
+                    stack.push(stack.pop() / divisor);
+                    break;
+                default:
+                    stack.push(Integer.parseInt(s));
             }
         }
-
-        return Integer.parseInt(expression.pop());
+        return stack.pop();
     }
 }
--- src/interviewbit/StacksAndQueues/RainWaterTrapped.java
+++ src/interviewbit/StacksAndQueues/RainWaterTrapped.java
@@ -3,26 +3,26 @@ package interviewbit.StacksAndQueues;
 import java.util.List;
 
 public class RainWaterTrapped {
-    public int trap(final List<Integer> A) {
-        int result = 0;
-        int n = A.size();
-        int leftMax = 0;
-        int rightMax = n - 1;
-        for (int i = 0, j = n - 1; i <= j; ) {
-            if (A.get(leftMax) <= A.get(rightMax)) {
-                if (A.get(i) < A.get(leftMax))
-                    result += A.get(leftMax) - A.get(i);
-                else
-                    leftMax = i;
-                i++;
-            } else {
-                if (A.get(j) < A.get(rightMax))
-                    result += A.get(rightMax) - A.get(j);
-                else
-                    rightMax = j;
-                j--;
-            }
-        }
-        return result;
-    }
+	public int trap(final List<Integer> A) {
+		int result = 0;
+		int leftMax = 0, rightMax = A.size() - 1;
+		for (int i = 0, j = A.size() - 1; i <= j; ) {
+			if (A.get(leftMax) <= A.get(rightMax)) {
+				if (A.get(i) < A.get(leftMax)) {
+					result += A.get(leftMax) - A.get(i);
+				} else {
+					leftMax = i;
+				}
+				i++;
+			} else {
+				if (A.get(rightMax) > A.get(j)) {
+					result += A.get(rightMax) - A.get(j);
+				} else {
+					rightMax = j;
+				}
+				j--;
+			}
+		}
+		return result;
+	}
 }
