828685ee528cb56b3d0afa426c264631f6e0a03b
Suhas EJ <suhas235@gmail.com>
2020-07-05 22:44:13 +0530

    InterviewBit Backtracking - Permutations

--- src/interviewbit/Backtracking/Permutations.java
+++ src/interviewbit/Backtracking/Permutations.java
@@ -1,27 +1,31 @@
 package interviewbit.Backtracking;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static java.util.Collections.swap;
 
 public class Permutations {
-    public ArrayList<ArrayList<Integer>> permute(ArrayList<Integer> A) {
-        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
-        helper(result, A, 0);
-        return result;
-    }
+	public static void main(String[] args) {
+		System.out.println(new Permutations().permute(new ArrayList<>(Arrays.asList(1, 2, 3))));
+	}
 
-    private void helper(ArrayList<ArrayList<Integer>> result, ArrayList<Integer> input, int index) {
-        if (index == input.size() - 1) {
-            result.add(new ArrayList<>(input));
-            return;
-        }
+	public ArrayList<ArrayList<Integer>> permute(ArrayList<Integer> A) {
+		ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+		generate(A, result, 0);
+		return result;
+	}
 
-        for (int i = index; i < input.size(); i++) {
-            swap(input, i, index);
-            helper(result, input, index + 1);
-            swap(input, i, index);
-        }
-    }
+	private void generate(ArrayList<Integer> a, ArrayList<ArrayList<Integer>> result, int index) {
+		if (index == a.size()) {
+			result.add(new ArrayList<>(a));
+			return;
+		}
+
+		for (int i = index; i < a.size(); i++) {
+			swap(a, index, i);
+			generate(a, result, index + 1);
+			swap(a, index, i);
+		}
+	}
 }
-
