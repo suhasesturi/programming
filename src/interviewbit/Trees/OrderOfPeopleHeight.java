a53715739d2b10d1e9a192adf0612e7896c7ce50
Suhas EJ <suhas235@gmail.com>
2020-07-24 23:12:07 +0530

    InterviewBit Trees - Segment Tree

--- src/interviewbit/Trees/OrderOfPeopleHeight.java
+++ src/interviewbit/Trees/OrderOfPeopleHeight.java
@@ -1,41 +1,28 @@
 package interviewbit.Trees;
 
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Arrays;
+import java.util.TreeMap;
 
 public class OrderOfPeopleHeight {
-    static class Person {
-        int height;
-        int inFront;
+	public ArrayList<Integer> order(ArrayList<Integer> heights, ArrayList<Integer> inFronts) {
+		TreeMap<Integer, Integer> map = new TreeMap<>();
+		ArrayList<Integer> result = new ArrayList<>();
 
-        Person(int height, int inFront) {
-            this.height = height;
-            this.inFront = inFront;
-        }
-    }
+		for (int i = 0; i < heights.size(); i++) {
+			map.put(heights.get(i), inFronts.get(i));
+			result.add(-1);
+		}
 
-    public ArrayList<Integer> order(ArrayList<Integer> heights, ArrayList<Integer> inFronts) {
-        ArrayList<Person> persons = new ArrayList<>();
-        ArrayList<Integer> result = new ArrayList<>();
-
-        for (int i = 0; i < heights.size(); i++) {
-            persons.add(new Person(heights.get(i), inFronts.get(i)));
-            result.add(-1);
-        }
-        persons.sort(Comparator.comparingInt(a -> a.height));
-
-        for (int i = 0; i < result.size(); i++) {
-            Person person = persons.get(i);
-            int count = person.inFront;
-            for (int j = 0; j < result.size(); j++) {
-                if (count == 0 && result.get(j) == -1) {
-                    result.set(j, person.height);
-                    break;
-                }
-                else if (result.get(j) == -1) count = Math.max(0, count - 1);
-            }
-        }
-
-        return result;
-    }
+		for (int height : map.keySet()) {
+			int count = map.get(height);
+			for (int i = 0; i < result.size(); i++) {
+				if (count <= 0 && result.get(i) == -1) {
+					result.set(i, height);
+					break;
+				} else if (result.get(i) == -1) count--;
+			}
+		}
+		return result;
+	}
 }
